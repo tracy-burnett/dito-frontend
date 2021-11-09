f9dcfbceebe271847f70fad2dfddd96232d7905f
Alexander McDowell <suppergear@gmail.com>
2021-11-09 18:27:49 +0000

    Update Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex justify-between items-center bg-indigo-600 px-5 py-2">
+        <div class="text-2xl text-white font-bold">Xygil</div>
+        <div class="flex">
+            <button class="bg-indigo-500 border border-indigo-400 text-sm px-3 py-1 my-1 rounded font-medium text-white hover:bg-indigo-400 transition-colors">LOGIN</button>
+            <img src="@/assets/menu-outline.svg" class="w-8 ml-6">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
