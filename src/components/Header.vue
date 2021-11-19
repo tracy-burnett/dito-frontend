a4b597a988ec5fb8ec7090b0490d8cae40e381fc
Alexander McDowell <suppergear@gmail.com>
2021-11-19 01:14:58 +0000

    Created Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,20 @@
 <template>
-    
+    <div class="flex flex-col w-screen hero py-12 lg:px-16 px-5">
+        <h1 class="text-white text-5xl font-extrabold mb-5">{{ title }}</h1>
+        <p class="text-gray-200 font-light text-sm lg:w-1/2 md:w-2/3 sm:w-full">{{ body }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "Header"
+    name: "Header",
+    props: ["title", "body", "image"]
 }
 </script>
 
 <style scoped>
-
+.hero {
+    background: #4F46E5;
+    background-size: cover;
+}
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <Header title="About" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper orci in pellentesque condimentum. Pellentesque eu sem ornare, commodo urna vitae, tempus felis. Aenean sit amet magna sed sapien pharetra tempus eu a sem. Duis eget est egestas mi sollicitudin mattis. Maecenas placerat nulla nec dui posuere varius. Donec ultricies nulla at velit aliquet ornare. Donec pharetra tempor auctor. Maecenas ultrices ante commodo mi egestas, nec dictum nibh tincidunt." />
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "About",
+  components: {
+    Navbar,
+    Header
+  },
+};
+</script>
