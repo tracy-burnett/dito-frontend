e54be5ea1d795c6aa25b31a7fb62bbc96ab1c2b7
skysnolimit08 <skysnolimit08@gmail.com>
2021-11-30 20:14:46 -0800

    Essay component for static pages

---
+++ src/components/Essay.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="lg:ml-16 mx-5">
+        <h1 class="font-bold text-2xl mt-8 mb-6">{{ title }}</h1>
+        <p class="text-indigo-200 font-light text-sm lg:w-2/3 md:w-3/4 sm:w-full">{{ body }}</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Essay",
+    props: ["title", "body"]
+}
+</script>
+
+<style scoped>
+</style>
