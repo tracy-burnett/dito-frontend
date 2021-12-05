426466956242e4182d70357f1957cbc144db4f16
Chrif20 <93154597+Chrif20@users.noreply.github.com>
2021-12-04 18:10:55 -0800

    Update Languagemenu.vue

--- src/components/Languagemenu.vue
+++ src/components/Languagemenu.vue
@@ -1,3 +1,31 @@
 <template>
 
+<div class="dropdown1" style="float:right;">
+  <button class="dropbtn1">Select Language 1</button>
+  <div class="dropdown-content1">
+  	  <a href="#">Language 1</a>
+      <a href="#">Language 2</a>
+      <a href="#">Language 3</a>
+      <a href="#">Language 4</a>
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+
+<div class="dropdown2" style="float:right;">
+  <button class="dropbtn2">Select Language 2</button>
+  <div class="dropdown-content2">
+  	  <a href="#">Language 1</a>
+      <a href="#">Language 2</a>
+      <a href="#">Language 3</a>
+      <a href="#">Language 4</a>
+  </div>
+</div>
+
 </template>
+
+<script>
+
+</script>
