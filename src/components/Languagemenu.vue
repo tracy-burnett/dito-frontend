ef07351128ec48202f88e13446c5ed6a4701b65f
Chrif20 <93154597+Chrif20@users.noreply.github.com>
2021-12-04 18:29:58 -0800

    Update Languagemenu.vue

--- src/components/Languagemenu.vue
+++ src/components/Languagemenu.vue
@@ -32,3 +32,47 @@ export default {
     props: ["text"]
 }
 </script>
+
+<style>
+.dropbtn {
+
+   background-color: #7833FF;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 16px;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  right: 0;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1;}
+.dropdown:hover .dropdown-content {display: block;}
+.dropdown:hover .dropbtn {background-color: #7833FF;}
+
+</style>
+
