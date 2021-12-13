e21041b6e857a8aa9381d2693161ffab146ade27
Howard Wang <howardwang00@gmail.com>
2021-12-12 19:34:26 -0800

    Audio Player: added current time and duration

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -4,7 +4,9 @@
             <button @click="play" class="play">
                 Play/Pause
             </button>
+            <div id="current" ref="current" class="song-length">0:00</div>
             <div id="waveform" class="waveform"></div>
+            <div id="length" ref="length" class="song-length">0:00</div>
         </div>
     </div>
     
@@ -20,6 +22,10 @@
 
 <script>
 
+function secondsToTime(seconds) {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
+}
+
 export default {
     name: "Player",
     components: {
@@ -34,11 +40,28 @@ export default {
             })
             // change this to load whatever audio file you want
             this.wavesurfer.load('https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3')
-        })
+            
+            const wavesurfer = this.wavesurfer;
+            this.wavesurfer.on('ready', function () {
+                document.getElementById('length').innerText = secondsToTime(wavesurfer.getDuration());
+            });
+            // this.$refs.length.innerText = new Date(audioLength * 1000).toISOString().substr(11, 8);
+            this.wavesurfer.on('audioprocess', function() {
+                // if (this.wavesurfer.isPlaying()) {
+                //     var currentTime = this.wavesurfer.getCurrentTime();
+                    
+                // }
+                // this.updateDuration();
+                document.getElementById('current').innerText = secondsToTime(wavesurfer.getCurrentTime());
+            });
+        });
     },
     methods: {
         play() {
             this.wavesurfer.playPause()
+        },
+        updateDuration() {
+            this.$refs.length.innerText = secondsToTime(this.wavesurfer.getDuration())
         }
     }
 }
@@ -51,10 +74,15 @@ export default {
 .waveform {
     /* flex: 1; */
     width: 100%;
-    margin-left: 10px;
-    margin-right: 10px;
+    /* margin-left: 10px; */
+    /* margin-right: 10px; */
 }
 .play {
     width: "fill-content";
+    padding: 10px;
+}
+.song-length {
+    margin: auto;
+    padding: 10px;
 }
 </style>
