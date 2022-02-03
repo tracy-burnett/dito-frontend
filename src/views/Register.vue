61e42afa4e2b347c39d3d7be6f780299900cb1cd
skysnolimit08 <skysnolimit08@gmail.com>
2022-02-02 17:27:15 -0800

    Debugging firebase code, reverting to v8 documentation

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,8 +7,8 @@
             <div class="flex flex-col items-center border border-gray-300 bg-white shadow-md rounded-xl p-8 xl:w-1/5 lg:w-1/4 md:w-1/3">
                 <h1 class="text-2xl font-bold">Register</h1>
                 <input class="border border-gray-300 rounded w-full mt-12 mb-3 px-3 py-1" placeholder="Email" v-model="email" />
-                <input class="border border-gray-300 rounded w-full px-3 py-1" placeholder="Password" v-model="password" />
-                <button class="bg-indigo-500 border border-indigo-400 w-full text-sm px-3 py-2 mt-16 rounded font-medium text-white hover:bg-indigo-400 transition-colors" @click="Register">Register</button>
+                <input class="border border-gray-300 rounded w-full px-3 py-1" placeholder="Password" type="password" v-model="password" />
+                <button class="bg-indigo-500 border border-indigo-400 w-full text-sm px-3 py-2 mt-16 rounded font-medium text-white hover:bg-indigo-400 transition-colors" @click="register">Register</button>
                 <router-link to="/login"><button class="bg-indigo-500 border border-indigo-400 w-full text-sm px-3 py-2 mt-16 rounded font-medium text-white hover:bg-indigo-400 transition-colors">Login</button></router-link>
             </div>
         </div>
@@ -20,51 +20,32 @@
 import Navbar from "@/components/Navbar.vue";
 import SidebarAlt from "@/components/SidebarAlt.vue";
 
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
-const auth = getAuth;
-
-
 export default {
     name: "Register",
     components: {
         Navbar,
         SidebarAlt
     },
+data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    register: function() {
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
 
-    data() {
-        return {
-        email: "",
-        password: ""
-        };
-    },
-
-    methods: {
-        register: function() {
-
-            createUserWithEmailAndPassword(auth, email, password)
-                .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                // ...
-                })
-                .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // ..
-                });
-        
-//      firebase
-//        .auth()
-//        .signInWithEmailAndPassword(this.email, this.password)
-//        .then(
-//          user => {
-//            this.$router.replace("Home");
-//          },
-//          err => {
-//            alert("Oops. " + err.message);
-//          }
-//        );
     }
   }
 }
