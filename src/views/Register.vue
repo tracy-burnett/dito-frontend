13b69eaa6131372774c8c650754fde550f9ab053
skysnolimit08 <skysnolimit08@gmail.com>
2022-02-02 17:40:58 -0800

    trying to fix Register form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,9 +6,11 @@
         <div class="flex justify-center items-center h-full mt-10">
             <div class="flex flex-col items-center border border-gray-300 bg-white shadow-md rounded-xl p-8 xl:w-1/5 lg:w-1/4 md:w-1/3">
                 <h1 class="text-2xl font-bold">Register</h1>
+                <form @submit.prevent="register">
                 <input class="border border-gray-300 rounded w-full mt-12 mb-3 px-3 py-1" placeholder="Email" v-model="email" />
                 <input class="border border-gray-300 rounded w-full px-3 py-1" placeholder="Password" type="password" v-model="password" />
-                <button class="bg-indigo-500 border border-indigo-400 w-full text-sm px-3 py-2 mt-16 rounded font-medium text-white hover:bg-indigo-400 transition-colors" @click="register">Register</button>
+                <button type="submit" class="bg-indigo-500 border border-indigo-400 w-full text-sm px-3 py-2 mt-16 rounded font-medium text-white hover:bg-indigo-400 transition-colors">Register</button>
+                </form>
                 <router-link to="/login"><button class="bg-indigo-500 border border-indigo-400 w-full text-sm px-3 py-2 mt-16 rounded font-medium text-white hover:bg-indigo-400 transition-colors">Login</button></router-link>
             </div>
         </div>
