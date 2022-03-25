6b06330f599c6dba0c63e64fb091be2c48f15c6b
= <lnavarret010@berkeley.edu>
2022-03-25 00:27:13 -0700

    changed player.vue

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,5 +1,6 @@
+
 <template>
-    <div class="container rounded-full shadow-lg items-center">
+    <div class="container rounded-full shadow-lg">
         <button id="play" @click="play" class="play">
             <div class="h-10 w-10">
                 <img v-if="playing" src="@/assets/pauseAudio.svg"/>
@@ -7,8 +8,15 @@
             </div>
         </button>
         <div id="current" ref="current" class="current">00:00:00</div>
+
+        <form role="form" name="edit">
+
+        <div id="start" ref="start" class="start"> <input type = "number" value ="0"> </div>
+        </form>
         <div id="waveform" class="waveform"></div>
-        <div id="length" ref="length" class="song-length">00:00:00</div>
+         <form role="form" name="edit">
+        <div id="length" ref="length" class="song-length"><input type = "number"></div>
+        </form>
     </div>
     
     <!-- <div class="lg:mx-16 mx-5">
@@ -23,13 +31,14 @@
 
 <script>
 
+
 function secondsToTime(seconds) {
     return new Date(seconds * 1000).toISOString().substr(11, 8);
 }
 
 export default {
     name: "Player",
-    props: ["audio"],
+    props: ["audio",  "region"],
     components: {
     },
     data: () => {
@@ -46,20 +55,42 @@ export default {
                 progressColor: '#5D5FEF',
                 barWidth: 2,
                 barRadius: 3,
-                height: 50,
-            });
-            
+                vertical: true,
+                plugins: [
+                    WaveSurfer.cursor.create({
+                        showTime: true,
+                        opacity: 1,
+                        customShowTimeStyle: {
+                            'background-color': '#000',
+                            color: '#fff',
+                            padding: '2px',
+                            'font-size': '10px'
+                            }    
+                        }),
+                    WaveSurfer.regions.create(),
+                    ]
+                });
             var audio = this.audio;
             if (!this.audio) {
                 // change this to load whatever audio file you want
-                audio = 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3'
+                audio = "https://archive.org/download/huck_finn_librivox/huckfinn_08_twain_apc.mp3"
+        
             }
             this.wavesurfer.load(audio);
             
+
             this.playing = false;
             const wavesurfer = this.wavesurfer;
+
             this.wavesurfer.on('ready', function () {
                 document.getElementById('length').innerText = secondsToTime(wavesurfer.getDuration());
+                region = wavesurfer.addRegion({
+                    start:0,
+                    end: wavesurfer.getDuration(),
+                    id: 'region',
+                    drag: false
+                     });
+                
             });
             // this.$refs.length.innerText = new Date(audioLength * 1000).toISOString().substr(11, 8);
             this.wavesurfer.on('audioprocess', function() {
@@ -70,6 +101,8 @@ export default {
                 // this.updateDuration();
                 document.getElementById('current').innerText = secondsToTime(wavesurfer.getCurrentTime());
             });
+            wavesurfer.on('region-click', editAnnotation);
+
         });
     },
     methods: {
@@ -85,18 +118,47 @@ export default {
         },
         updateDuration() {
             this.$refs.length.innerText = secondsToTime(this.wavesurfer.getDuration())
-        }
+        },
+      
     }
 }
+function editAnnotation(region) {
+    let form = document.forms.edit;
+    console.log(region);
+    console.log(form);
+    form.style.opacity = 1;
+    (form.elements.start.value = Math.round(region.start * 10) / 10),
+    (form.elements.end.value = Math.round(region.end * 10) / 10);
+    form.elements.note.value = region.data.note || '';
+    form.onsubmit = function(e) {
+        e.preventDefault();
+        region.update({
+            start: form.elements.start.value,
+            end: form.elements.end.value,
+            data: {
+                note: form.elements.note.value
+            }
+        });
+        form.style.opacity = 0;
+    };
+    form.onreset = function() {
+        form.style.opacity = 0;
+        form.dataset.region = null;
+    };
+    form.dataset.region = region.id;
+}  
 </script>
 
 <style scoped>
 .container {
-    display: flex;
+   display: inline;
+   max-width: 128px;
+   text-align: center;
 }
 .waveform {
     /* flex: 1; */
-    width: 100%;
+    display: flex;
+    height: 410px;
     /* margin-left: 10px; */
     /* margin-right: 10px; */
 }
@@ -113,4 +175,8 @@ export default {
 .song-length {
     padding: 5px;
 }
+input{
+    box-sizing:border-box;
+    } 
+
 </style>
