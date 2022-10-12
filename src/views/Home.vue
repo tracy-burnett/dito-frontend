684c44302135d3e31cec9b6e264b4cb51106679c
skysnolimit08 <skysnolimit08@gmail.com>
2022-10-12 12:00:11 -0700

    change blurb to make it universally applicable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="flex-auto">
     <div class="flex flex-col">
       <Navbar />
-      <Header title="Dito" image ="src/assets/ditologo.jpg" body="A platform for developing useful teaching tools for language exchange and typing practice between Khroskyabs and English." />
+      <Header title="Dito" image ="src/assets/ditologo.jpg" body="A platform for developing useful teaching tools for language exchange, listening comprehension, reading comprehension, and typing practice." />
       <PublicCardList title="Browse Public Storybooks" />
     </div>
   </div>
