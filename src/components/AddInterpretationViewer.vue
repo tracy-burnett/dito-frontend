95fdbb560ec97022a3af27c30dddd8b1fc45b3a2
skysnolimit08 <skysnolimit08@gmail.com>
2022-11-10 09:31:17 -0800

    change some font size of right-hand side buttons for adding new interpretations

--- src/components/AddInterpretationViewer.vue
+++ src/components/AddInterpretationViewer.vue
@@ -4,19 +4,19 @@
     <div  class="fixed  mt-[26vh]" id="create" @click="show" style="cursor:pointer">
       <div class="w-[3.4vw]">
         <img src="@/assets/add-new.svg" />
-      </div> Create New<br>Interpretation
+      </div> <p class="md:text-base sm:text-sm">Create New<br>Interpretation</p>
     </div>
         <!-- this is a button for creating a new Interpretation and having it displayed in its own column -->
         <div  class="fixed mt-[46vh]" id="upload" @click="upload" style="cursor:pointer">
       <div class="w-[3.4vw]">
         <img src="@/assets/add-new.svg" />
-      </div> Upload .srt<br>Interpretation
+      </div><p class="md:text-base sm:text-sm"> Upload .srt<br>Interpretation</p>
     </div>
     <!-- this is a button for adding another column that shows an Interpretation that you have access to but aren't yet viewing -->
     <div  class="fixed  mt-[66vh]" v-show="this.interpretationsList[0]" id="add" @click="add" style="cursor:pointer">
       <div class="w-[3.4vw]">
         <img src="@/assets/add-existing.svg" />
-      </div> Add Another<br>Console
+      </div> <p class="md:text-base sm:text-sm">Add Another<br>Console</p>
     </div>
     <br />
   </div>
