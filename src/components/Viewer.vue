588ffaf69231b5f27324370f32dd4a3472a20564
skysnolimit08 <skysnolimit08@gmail.com>
2022-11-16 09:54:29 -0800

    adjust Viewer and Prompter so that they can better represent or manage nested phrases

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -24,15 +24,22 @@ also, if the user clicks on the text of that substring, snap the audio player to
 					:key="substring.startingcharacter"
 					ref="highlightedwords"
 				>
+				<span
+						v-show="highlight(substring.startingcharacter)==2"
+						class="text-blue-600"
+						style="white-space: pre-wrap"
+						@click="snapToTimestamp(substring.startingcharacter)"
+					>{{ substring.text }}</span>
+
 					<span
-						v-show="highlight(substring.startingcharacter)"
+						v-show="highlight(substring.startingcharacter)==1"
 						class="text-red-600"
 						style="white-space: pre-wrap"
 						@click="snapToTimestamp(substring.startingcharacter)"
 					>{{ substring.text }}</span>
 
 					<span
-						v-show="!highlight(substring.startingcharacter)"
+						v-show="highlight(substring.startingcharacter)==0"
 						class="cursor-pointer"
 						@click="snapToTimestamp(substring.startingcharacter)"
 						style="white-space: pre-wrap"
