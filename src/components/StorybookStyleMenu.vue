4374114b9c23378cbd8769b53a8541e97585c8c1
skysnolimit08 <skysnolimit08@gmail.com>
2022-12-24 16:05:00 -0800

    try to fix missing component StorybookStyleMenu from some interpretations sometimes

--- src/components/StorybookStyleMenu.vue
+++ src/components/StorybookStyleMenu.vue
@@ -86,10 +86,10 @@
 					src="@/assets/icon_help.svg"
 					style="filter: brightness(0) invert(1)"
 				/></button>
-			<!-- <button
+			<button
 				v-else
 				class="border-sky-600 bg-sky-700 hover:bg-sky-600 dropbtn px-[.6vw] py-[1vh]"
-			>Interaction</button> -->
+			>Interaction</button>
 			<div class="dropdown-content">
 
 				<div class="absolute right-0 mr-[1vw] grid h-full">
