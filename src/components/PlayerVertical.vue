92bd0818547e09015d3cd6bab06ea7d12847ad1f
skysnolimit08 <skysnolimit08@gmail.com>
2023-02-13 12:04:07 -0800

    fix rounding error

--- src/components/PlayerVertical.vue
+++ src/components/PlayerVertical.vue
@@ -759,16 +759,19 @@ export default {
 				// when the player starts playing, make sure it plays from whenever is currently displayed in the "current time" box that the user is also able to manually inpput into, unless of course that value is outside of the highlighted region
 				if (
 					this.currentTimeSeconds < this.endTimeSeconds &&
-					Math.round(this.currentTimeSeconds)/100 >= this.startTimeSeconds
+					Math.round(this.currentTimeSeconds*100)/100 >= this.startTimeSeconds
 				) {
 					// console.log("playing inside region");
 					// console.log(this.startTimeSeconds)
-					// console.log(this.currentTimeSeconds)
+					// console.log(Math.round(this.currentTimeSeconds*100)/100)
 					// console.log(this.endTimeSeconds)
 					this.wavesurfer.play(this.currentTimeSeconds);
 					this.playing = !this.playing;
 				} else {
 					// console.log("playing from start of region");
+					// console.log(this.startTimeSeconds)
+					// console.log(Math.round(this.currentTimeSeconds*100)/100)
+					// console.log(this.endTimeSeconds)
 					this.wavesurfer.play(this.startTimeSeconds);
 					this.playing = !this.playing;
 				}
