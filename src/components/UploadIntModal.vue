5f0c0545b237b295c62f76396375ed2d967bee50
skysnolimit08 <skysnolimit08@gmail.com>
2023-04-12 17:32:03 -0700

    EAF reader is getting better.  includes ref annotations now.  haven't tested whether I broke the other uploaders yet.  Also, moved int_text out of computed property into a method because it stopped firing in successive rounds of parsing the EAF xml stuff.

--- src/components/UploadIntModal.vue
+++ src/components/UploadIntModal.vue
@@ -7,8 +7,8 @@
 			</button>
 			<h1 class="text-2xl font-bold mb-[1vh]">Upload Interpretation File</h1>
 			<br />
-			<input class="w-full px-3 py-[1vh] mb-[2vh] border border-gray-300 rounded" type="file" accept=".srt, .txt, .eaf"
-				ref="interpretationInput" />
+			<input class="w-full px-3 py-[1vh] mb-[2vh] border border-gray-300 rounded" type="file"
+				accept=".srt, .txt, .eaf" ref="interpretationInput" />
 			<select v-model="filetype" class="w-full px-3 py-[1vh] mb-[1vh] border border-gray-300 rounded"
 				:class="{ 'text-gray-500': isActive }">
 				<option value="">What file format are you uploading?</option>
@@ -16,11 +16,11 @@
 				<option value="eaf" class="text-black">ELAN Annotation Format (.eaf)</option>
 				<option value="tsv" class="text-black">Audacity Timing File (.txt)</option>
 			</select>
-			<input class="w-full px-3 py-[1vh] border border-gray-300 rounded" placeholder="Title of New Interpretation"
-				v-model="int_title" />
-			<input class="w-full px-3 py-[1vh] border border-gray-300 rounded" placeholder="Language of New Interpretation"
-				v-model="int_language" />
-			<input class="w-full px-3 py-[1vh] border border-gray-300 rounded"
+			<input v-if="filetype != 'eaf'" class="w-full px-3 py-[1vh] border border-gray-300 rounded"
+				placeholder="Title of New Interpretation" v-model="int_title" />
+			<input v-if="filetype != 'eaf'" class="w-full px-3 py-[1vh] border border-gray-300 rounded"
+				placeholder="Language of New Interpretation" v-model="int_language" />
+			<input v-if="filetype != 'eaf'" class="w-full px-3 py-[1vh] border border-gray-300 rounded"
 				placeholder="What character is this language 'spaced' by? (or leave blank)" v-model="int_spacing"
 				maxlength="1" />
 			<br>
@@ -44,11 +44,13 @@ export default {
 		return {
 			int_title: "",
 			int_text_unstripped: "",
+			int_text: "",
 			int_language: "",
 			fileloaded: "",
 			filetype: "",
 			int_spacing: "",
 			file: null,
+			timestamps: {},
 			captions: [],
 			// maxTimestampArray: [], // to make sure none of them exceed the length of the audio file
 			timestampsforBackend: [],
@@ -69,17 +71,7 @@ export default {
 			else { return false }
 		},
 
-		int_text() {
-			if (this.int_spacing != "") {
-				let stripped = this.int_text_unstripped.replace(
-					this.regexwithmultiplespacedby,
-					this.int_spacing
-				);
-				return stripped;
-			} else if (this.int_spacing == "") {
-				return this.int_text_unstripped;
-			}
-		},
+
 
 		regexwithspacedby() {
 			return new RegExp(
@@ -91,6 +83,14 @@ export default {
 		},
 	},
 	watch: {
+		filetype: function () {
+			if (this.filetype == "eaf") {
+				this.int_language = ""
+				this.int_title = ""
+				this.int_spacing = ""
+			}
+		},
+
 		fileloaded: function () {
 			if (this.fileloaded == "") { }
 			else if (this.filetype == "srt") {
@@ -122,6 +122,19 @@ export default {
 		},
 	},
 	methods: {
+
+		getIntText() {
+			if (this.int_spacing != "") {
+				let stripped = this.int_text_unstripped.replace(
+					this.regexwithmultiplespacedby,
+					this.int_spacing
+				);
+				this.int_text = stripped;
+			} else if (this.int_spacing == "") {
+				this.int_text = this.int_text_unstripped;
+			}
+		},
+
 		escapeRegex: function (string) {
 			return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
 		},
@@ -160,7 +173,12 @@ export default {
 				this.int_language != ""
 			) {
 				// console.log(this.int_text.normalize("NFC"))
-				fetch(
+				// console.log(this.int_title.normalize("NFC"))
+				// console.log(this.int_text.normalize("NFC"))
+				// console.log(this.int_language.normalize("NFC"))
+				// console.log(this.int_spacing.normalize("NFC"))
+
+				let response = await fetch(
 					process.env.VUE_APP_api_URL +
 					"interpretations/audio/" +
 					this.audio_id +
@@ -188,40 +206,45 @@ export default {
 						// console.log(response)
 						//add in the association for the new phrase.
 						// console.log(this.new_associations)
-						fetch(
-							process.env.VUE_APP_api_URL +
-							"content/" +
-							this.audio_id +
-							"/" +
-							response.interpretation.id,
-							{
-								method: "POST",
-								body: JSON.stringify({
-									// text: this.latest_text.normalize('NFC'), // Pass in a string that meets a minimum character fcount and includes all the new tags you want to save
-									associations: this.new_associations, // Pass in the list of the new tags
-								}),
 
-								headers: {
-									"Content-Type": "application/json",
+						return response
 
-									Authorization: this.$store.state.idToken,
-								},
-							}
-						)
-							.then(() => {
-								this.$emit("addCreatedInterpretation", response.interpretation);
-								this.$emit("closeUploadIntModal");
-							})
-							// .then((data) => console.log(data))
-							.catch((error) => console.error("Error:", error));
-						// if the interpretation was created successfully, then tell the parent component to add it to the list of interpretations potentially displayed in the dropdown menu, and tell the Vuex store that we need to add another column to the main screen for viewing the new interpretation
-
-						return;
 					})
 
 					.catch((error) => {
 						console.error("Error:", error);
 					});
+
+
+				await fetch(
+					process.env.VUE_APP_api_URL +
+					"content/" +
+					this.audio_id +
+					"/" +
+					response.interpretation.id,
+					{
+						method: "POST",
+						body: JSON.stringify({
+							// text: this.latest_text.normalize('NFC'), // Pass in a string that meets a minimum character fcount and includes all the new tags you want to save
+							associations: this.new_associations, // Pass in the list of the new tags
+						}),
+
+						headers: {
+							"Content-Type": "application/json",
+
+							Authorization: this.$store.state.idToken,
+						},
+					}
+				)
+					.then(() => {
+						this.$emit("addCreatedInterpretation", response.interpretation);
+						this.$emit("closeUploadIntModal");
+					})
+					// .then((data) => console.log(data))
+					.catch((error) => console.error("Error:", error));
+				// if the interpretation was created successfully, then tell the parent component to add it to the list of interpretations potentially displayed in the dropdown menu, and tell the Vuex store that we need to add another column to the main screen for viewing the new interpretation
+
+				return;
 			}
 		},
 
@@ -244,16 +267,134 @@ export default {
 		},
 
 		async eafToInterpretation() {
-			this.timestampsforBackend.length = 0;
-			this.offsetsforBackend.length = 0;
-			this.captions.length = 0;
-			await this.eafToInterpretationHelper()
-			this.prepText()
+			for (var member in this.timestamps) delete this.timestamps[member]
+
+			let xmlDoc = new DOMParser().parseFromString(this.fileloaded, "text/xml")
+
+			// console.log(xmlDoc)
+
+
+			let time_unit = xmlDoc.querySelector("HEADER").getAttribute('TIME_UNITS')
+			if (time_unit != "milliseconds") { throw new Error('Dito can only upload .eaf files with timestamps calculated in milliseconds at this time.') }
+			this.timestamps = {}
+			let timeslots = xmlDoc.querySelector("TIME_ORDER").querySelectorAll("TIME_SLOT")
+
+			// console.log(timeslots)
+
+			for (let a = 0; a < timeslots.length; a++) {
+				this.timestamps[timeslots[a].getAttribute("TIME_SLOT_ID")] = timeslots[a].getAttribute("TIME_VALUE")
+			}
+
+			// console.log(this.timestamps)
+
+			let eafObject = await this.eafToInterpretationHelper(xmlDoc)
+			console.log(eafObject)
+
+
+			let arrayOfTierNames = Object.keys(eafObject)
+
+
+			for (let m = 0; m < arrayOfTierNames.length; m++) {
+
+				this.timestampsforBackend.length = 0;
+				this.offsetsforBackend.length = 0;
+				this.captions.length = 0;
+
+				// console.log(this.int_text_unstripped)
+				this.int_title = arrayOfTierNames[m]
+				console.log(this.int_title)
+				this.int_spacing = " "
+				this.int_text_unstripped = ""
+				this.getIntText()
+				// console.log(this.int_text_unstripped)
+				// console.log(this.int_text)
+				this.int_language = ""
+				let arrayToParse = Object.entries(eafObject[arrayOfTierNames[m]])
+				console.log(arrayToParse)
+
+				for (let j = arrayToParse.length - 1; j >= 0; j--) {
+					if (arrayToParse[j][1]["annotationText"] == "") {
+						arrayToParse.splice(j, 1);
+					}
+				}
+				console.log(arrayToParse)
+
+				let lastEndSeconds = 0;
+				for (let h = 0; h < arrayToParse.length; h++) {
+					let srt_instructions = arrayToParse[h][1]["annotationText"]
+					// console.log(srt_instructions)
+
+					let timestampStartReformatted = 0
+					let timestampEndReformatted = 0
+					if (arrayToParse[h][1]["annotationType"] == "alignable") {
+						timestampStartReformatted = Number(this.timestamps[arrayToParse[h][1]["timeslotRef1"]]) / 1000
+						timestampEndReformatted = Number(this.timestamps[arrayToParse[h][1]["timeslotRef2"]]) / 1000
+						// console.log(timestampStartReformatted)
+						// console.log(timestampEndReformatted)
+					}
+					else if (arrayToParse[h][1]["annotationType"] == "ref") {
+						timestampStartReformatted = Number(this.timestamps[eafObject[arrayToParse[h][1]["parentRef"]][arrayToParse[h][1]["annotationRef"]]["timeslotRef1"]]) / 1000
+						timestampEndReformatted = Number(this.timestamps[eafObject[arrayToParse[h][1]["parentRef"]][arrayToParse[h][1]["annotationRef"]]["timeslotRef2"]]) / 1000
+					}
+					lastEndSeconds = await this.formatForBackend(timestampStartReformatted, timestampEndReformatted, srt_instructions, lastEndSeconds)
+
+				}
+				await this.prepText()
+			}
+
+
+
+
 		},
 
-		async eafToInterpretationHelper() {
-			console.log(this.fileloaded)
-			debugger
+		async eafToInterpretationHelper(xmlDoc) {
+			// console.log(this.fileloaded)
+
+
+
+			let tiers = xmlDoc.querySelectorAll("TIER")
+
+			let newInterpretations = {}
+
+			tiers.forEach(tier => {
+				let parentRef = tier.getAttribute("PARENT_REF")
+				// let annotationType = tier
+				// console.log(tier.querySelector("ANNOTATION").firstElementChild.tagName)
+				// console.log(parentRef)
+				newInterpretations[tier.getAttribute("TIER_ID")] = {}
+				if (tier.querySelector("ANNOTATION") && tier.querySelector("ANNOTATION").firstElementChild.tagName == "ALIGNABLE_ANNOTATION") {
+					tier.querySelectorAll("ANNOTATION").forEach(annotation => {
+						let child = annotation.firstElementChild
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")] = {}
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")].annotationType = "alignable"
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")].timeslotRef1 = child.getAttribute("TIME_SLOT_REF1")
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")].timeslotRef2 = child.getAttribute("TIME_SLOT_REF2")
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")].annotationText = child.firstElementChild.textContent
+
+						// console.log("alignable: " + annotationId + "; " + timeslotRef1 + "; " + timeslotRef2 + "; " + annotationText)
+					})
+				} else if (tier.querySelector("ANNOTATION") && tier.querySelector("ANNOTATION").firstElementChild.tagName == "REF_ANNOTATION") {
+					// newInterpretations[tier.getAttribute("TIER_ID")].forEach(annotation => {
+					tier.querySelectorAll("ANNOTATION").forEach(annotation => {
+						let child = annotation.firstElementChild
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")] = {}
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")].parentRef = parentRef
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")].annotationType = "ref"
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")].annotationRef = child.getAttribute("ANNOTATION_REF")
+						newInterpretations[tier.getAttribute("TIER_ID")][child.getAttribute("ANNOTATION_ID")].annotationText = child.firstElementChild.textContent
+
+						// console.log("ref: " + annotationId + "; " + parentRef + "; " + annotationRef + "; " + annotationText)
+					})
+
+
+				}
+
+			}
+			)
+			return newInterpretations
+
+
+
 		},
 
 		async srtToInterpretationHelper() {
@@ -271,6 +412,8 @@ export default {
 			for (let h = 0; h < arrayToParse.length; h++) {
 
 				let srt_instructions = arrayToParse[h].trim().split("\n");
+				srt_instructions.shift();
+				srt_instructions.shift();
 
 				let timestampInstructions = srt_instructions[1];
 
@@ -311,6 +454,8 @@ export default {
 			let lastEndSeconds = 0;
 			for (let h = 0; h < arrayToParse.length; h++) {
 				let srt_instructions = arrayToParse[h].trim().split("\t");
+				srt_instructions.shift();
+				srt_instructions.shift();
 				let timestampStartReformatted = Number(srt_instructions[0])
 				let timestampEndReformatted = Number(srt_instructions[1])
 				lastEndSeconds = await this.formatForBackend(timestampStartReformatted, timestampEndReformatted, srt_instructions, lastEndSeconds)
@@ -320,13 +465,11 @@ export default {
 
 		formatForBackend(timestampStart, timestampEnd, srt_instructions, lastEndSeconds) {
 			// only add captions that don't exceed the maximum length of the audio file
-			// console.log((1000 * timestampEnd) + " should be less than or equal to " + this.$store.state.audioDuration)
-			// console.log((1000*timestampStart + " should ge greater than or equal to 0"))
-			// console.log(srt_instructions[2] + " should not be undefined")
+
 			if (
 				1000 * timestampEnd <= this.$store.state.audioDuration &&
 				1000 * timestampStart >= 0 &&
-				srt_instructions[2] != undefined
+				srt_instructions != undefined
 			) {
 				let timestampforBackend =
 					(100 *
@@ -340,15 +483,16 @@ export default {
 							timestampStart)) /
 					2;
 				this.offsetsforBackend.push(offsetforBackend);
-				srt_instructions.shift();
-				srt_instructions.shift();
 				let caption_text = "";
-				srt_instructions.forEach(
-					// this is for if the caption takes up more than one line in the text file
-					(part) => (caption_text = caption_text + "\n" + part)
-				);
+				if (Array.isArray(srt_instructions)) {
+					srt_instructions.forEach(
+						// this is for if the caption takes up more than one line in the text file
+						(part) => (caption_text = caption_text + "\n" + part)
+					);
+				} else { caption_text = "\n" + srt_instructions }
 
-				console.log((100 * timestampStart) + " should be greater than " + lastEndSeconds)
+				// console.log((100 * timestampStart) + " should be greater than " + lastEndSeconds)
+				// console.log(caption_text)
 
 				if (
 					100 *
@@ -364,18 +508,24 @@ export default {
 				lastEndSeconds =
 					100 * timestampEnd;
 				return lastEndSeconds
-			} else { return lastEndSeconds }
+			} else {
+				console.log((1000 * timestampEnd) + " should be less than or equal to " + this.$store.state.audioDuration)
+				console.log((1000 * timestampStart + " should be greater than or equal to 0"))
+				console.log(srt_instructions + " should not be undefined")
+				return lastEndSeconds
+			}
 		},
 
-		prepText() {
-
-			if (this.captions[0][0] == "\n" && this.captions[0][1] == "\n") {
+		async prepText() {
+			// console.log(...this.captions)
+			if (this.captions.length>1 && this.captions[0][0] == "\n" && this.captions[0][1] == "\n") {
 				// take away the first two carriage returns from before the first caption
 				// console.log("HIT")
 				// console.log(this.captions[0])
 				this.captions[0] = this.captions[0].slice(2);
 				// console.log(this.captions[0])
 			}
+
 
 			// console.log(this.captions);
 			// for (let a=0; a < this.captions.length; a++)
@@ -438,10 +588,14 @@ export default {
 					this.captions[b] = this.int_spacing + this.captions[b];
 				}
 			}
+
 			this.int_text_unstripped = this.captions.join("");
+			this.getIntText()
 			// console.log(this.int_text_unstripped);
 			// console.log(this.int_text);
 			this.captions_cleaned.length = 0;
+			
+			for (var member in this.new_associations) delete this.new_associations[member]
 			// console.log(this.captions);
 
 			this.captions.forEach((caption) => {
@@ -469,9 +623,11 @@ export default {
 			// console.log(this.timestampsforBackend);
 
 			let wordindexcount = 0;
+			// console.log(this.captions_cleaned)
 			this.captions_cleaned.forEach((caption, captionindex) => {
 				for (let j = 0; j < caption.length; j++) {
 					if (caption[j] != "\n") {
+						// console.log(caption[j])
 						this.new_associations[wordindexcount] = {};
 						this.new_associations[wordindexcount][
 							Math.round(this.timestampsforBackend[captionindex])
@@ -484,7 +640,7 @@ export default {
 
 			// console.log(this.new_associations);
 
-			this.create();
+			await this.create();
 		},
 
 
