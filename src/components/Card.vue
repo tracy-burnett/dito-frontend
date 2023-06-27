750c67f10660a6d8a263bed132b22ef08cb23b6d
skysnolimit08 <skysnolimit08@gmail.com>
2023-06-27 15:16:16 -0700

    remove storybook cover images

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,33 +1,34 @@
 <template>
-	<div
-		@click="openstorybook()"
-		class="relative mb-[3vh] overflow-hidden transition-colors bg-white border-slate-300 border shadow-lg cursor-pointer hover:overflow-auto card rounded-xl  hover:border-cyan-600"
-	>
-		<img
-			class="object-cover w-full h-1/2"
-			:src="image"
-		/>
-		<div class="px-4 py-2">
+	<div @click="openstorybook()"
+		class="relative mb-[3vh] overflow-hidden transition-colors bg-white border-slate-300 border shadow-lg cursor-pointer hover:overflow-auto group card rounded-xl  hover:border-teal-50">
+		<div class="relative flex items-center justify-center bg-emerald-800 flex-cols-2">
+			<div class="absolute inset-0 z-10 opacity-0 bg-emerald-600 group-hover:opacity-100"></div>
+			<!-- <div class=""> -->
+			<!-- <img class="flex left-0 mt-[1vh] object-scale-down  w-1/3" :src="image" /> -->
 			<div
-				class="mt-1 mb-2 text-xl font-bold"
-				ref="storybooktitle"
-			>{{ title }}</div>
+				class=" z-20 my-[1vh] ml-[-.5vw] flex items-center justify-center w-[3vw] h-[3vw] mr-4 rounded-full shadow-lg bg-cyan-700">
+				<img class="w-[2vw] h-[2vw] ml-1" src="@/assets/play.svg" />
+			</div>
+
+			<div class="z-20 flex -ml-[1vw] mt-1 mb-2 text-xl text-gray-100">&nbsp;&nbsp;play storybook</div>
+			<!-- </div> -->
+		</div>
+		<!-- <hr class="mt-[1.4vh] border-lime-500"> -->
+
+		<div class="px-4 py-2">
+			<div class="mt-1 mb-2 text-xl font-bold " ref="storybooktitle">{{ title }}</div>
 			<p class="mb-3 text-xs text-gray-400">
 				{{ description }}
 			</p>
 			<label class="text-xs text-gray-400">Created by {{ uploader }} on {{ date }}</label>
 		</div>
-		<div class="absolute flex items-center justify-center mr-4 rounded-full shadow-lg bg-sky-700 h-14 w-14 play">
-			<img
-				class="w-8 h-8 ml-1 text-white"
-				src="@/assets/play.svg"
-			/>
-		</div>
+
 	</div>
 </template>
 
 <script>
-import cardbanner from "@/assets/IMG_0478.jpg";
+// import cardbanner from "@/assets/IMG_0478.jpg";
+// import browse from "@/assets/icon_detailed_browse.svg";
 
 export default {
 	name: "Card",
@@ -37,9 +38,9 @@ export default {
 			default: "",
 		},
 
-		image: {
-			default: cardbanner,
-		},
+		// image: {
+		// 	default: browse,
+		// },
 
 		description: {
 			default: "",
@@ -79,16 +80,20 @@ export default {
 	margin-left: 0.5vw;
 	margin-right: 0.5vw;
 	height: 27.5vh;
-	-ms-overflow-style: none; /* for Internet Explorer, Edge */
-	scrollbar-width: none; /* for Firefox */
+	-ms-overflow-style: none;
+	/* for Internet Explorer, Edge */
+	scrollbar-width: none;
+	/* for Firefox */
 	overflow-y: scroll;
 }
 
 .card::-webkit-scrollbar {
-	display: none; /* for Chrome, Safari, and Opera */
+	display: none;
+	/* for Chrome, Safari, and Opera */
 }
-.play {
+
+/* .play {
 	top: calc(50% - 1.75rem);
 	right: 0;
-}
+} */
 </style>
