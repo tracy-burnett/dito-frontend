7039024e527a6d92dbe7460256795602cb4ac7c2
skysnolimit08 <skysnolimit08@gmail.com>
2023-06-27 15:43:31 -0700

    updated Cards a little bit

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,22 +1,26 @@
 <template>
 	<div @click="openstorybook()"
-		class="relative mb-[3vh] overflow-hidden transition-colors bg-white border-slate-300 border shadow-lg cursor-pointer hover:overflow-auto group card rounded-xl  hover:border-teal-50">
-		<div class="relative flex items-center justify-center bg-emerald-800 flex-cols-2">
-			<div class="absolute inset-0 z-10 opacity-0 bg-emerald-600 group-hover:opacity-100"></div>
+		class="relative mb-[3vh] overflow-hidden transition-colors bg-white border-slate-300 border shadow-lg cursor-pointer hover:overflow-auto group card rounded-xl  hover:border-slate-400 hover:outline-2">
+		<div class="relative flex items-center justify-between bg-emerald-800 flex-cols-2">
+			<div class="absolute inset-0 z-10 opacity-0 bg-emerald-900 group-hover:opacity-100"></div>
 			<!-- <div class=""> -->
 			<!-- <img class="flex left-0 mt-[1vh] object-scale-down  w-1/3" :src="image" /> -->
+
+			<div class="z-20 flex mt-1 ml-[1vw] mb-2 text-xl text-gray-100 group-hover:text-gray-50">&nbsp;&nbsp;{{ title
+			}}</div>
 			<div
-				class=" z-20 my-[1vh] ml-[-.5vw] flex items-center justify-center w-[3vw] h-[3vw] mr-4 rounded-full shadow-lg bg-cyan-700">
-				<img class="w-[2vw] h-[2vw] ml-1" src="@/assets/play.svg" />
+				class="relative z-20 my-[1vh] flex items-center justify-center w-[3vw] h-[3vw] mr-[1vw] rounded-full shadow-lg bg-cyan-700">
+				<div class="absolute inset-0 z-30 rounded-full shadow-lg opacity-0 bg-cyan-800 group-hover:opacity-100">
+				</div>
+				<img class="z-40 w-[2vw] h-[2vw] ml-1" src="@/assets/play.svg" />
 			</div>
 
-			<div class="z-20 flex -ml-[1vw] mt-1 mb-2 text-xl text-gray-100">&nbsp;&nbsp;play storybook</div>
 			<!-- </div> -->
 		</div>
 		<!-- <hr class="mt-[1.4vh] border-lime-500"> -->
 
 		<div class="px-4 py-2">
-			<div class="mt-1 mb-2 text-xl font-bold " ref="storybooktitle">{{ title }}</div>
+			<!-- <div class="mt-1 mb-2 text-xl font-bold " ref="storybooktitle">{{ title }}</div> -->
 			<p class="mb-3 text-xs text-gray-400">
 				{{ description }}
 			</p>
@@ -59,9 +63,9 @@ export default {
 		},
 	},
 
-	mounted() {
-		this.$refs.storybooktitle.scrollIntoView({ block: "center" });
-	},
+	// mounted() {
+		// this.$refs.storybooktitle.scrollIntoView({ block: "center" });
+	// },
 
 	methods: {
 		openstorybook() {
