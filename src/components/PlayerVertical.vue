60a8695dfc56d22cf604f9b0ac1a8e7cccb7a51a
skysnolimit08 <59749584+skysnolimit08@users.noreply.github.com>
2023-08-20 18:56:52 +0800

    Update PlayerVertical.vue

--- src/components/PlayerVertical.vue
+++ src/components/PlayerVertical.vue
@@ -9,7 +9,6 @@
 </template>
 
 <script>
-import { getIdToken } from "firebase/auth";
 
 export default {
 	// name of component
@@ -28,26 +27,7 @@ export default {
 	// declare the variables and default values that this component will need
 	data: () => {
 		return {
-			playbackspeed: 1,
-			isLoaded: false,
-			loadingpercent: 0,
-			readyVerification: 0,
-			// peaksData: [],
-			zoomnumber: 0,
-			sendtobackendBoolean: false,
-			startTime: "00:00:00", // the beginning of the highlighted region as calculated by wavesurfer OR manually input by the user, in HH:MM:SS
-			currentTime: "00:00:00", // wherever the audio is currently playing as calculated by wavesurfer OR manually input by the user, in HH:MM:SS
-			endTime: "00:00:00", // the end of the highlighted region as calculated by wavesurfer OR manually input by the user, in HH:MM:SS
-			startTimeSeconds: 0, // the beginning of the highlighted region as calculated by wavesurfer, in seconds
-			currentTimeSeconds: 0, // wherever the audio is currently playing as calculated by wavesurfer, in seconds
-			endTimeSeconds: 0, // the end of the highlighted region as calculated by wavesurfer, in seconds
-			totalDuration: 0, // the total length of the audio, in seconds
-			playing: false,
 			audioURL: "",
-			updatingFromPrompter: false,
-			repeat: false,
-			hasRegion: false,
-			autoscroll: true,
 		};
 	},
 
@@ -63,63 +43,22 @@ export default {
 			container: this.$refs.waveform,
 			waveColor: "#94a3b8",
 			cursorColor: "red",
-			// autoCenter: false,
 			progressColor: "#475569",
-			// barWidth: 2,
 			barHeight: 4,
-			// normalize: true,
 			hideScrollbar: true,
-			// barRadius: 3,
 			vertical: true,
 		});
 
+		this.audioURL = "https://tile.loc.gov/storage-services/media/afc/cal/afc1986022_sr09b02.mp3"
 
-		if (this.$store.state.user) {
-			// REFRESH ID TOKEN FIRST AND WAIT FOR IT
-			await getIdToken(this.$store.state.user)
-				.then((idToken) => {
-					this.$store.commit("SetIdToken", idToken);
-					// console.log(this.$store.state.idToken)
-				})
-				.catch((error) => {
-					// An error happened.
-					console.log("Oops. " + error.code + ": " + error.message);
-				});
-		}
-
-		//get secure url from server
-		const apiUrl = process.env.VUE_APP_api_URL + "s3/presignedgeturl";
-		fetch(apiUrl, {
-			method: "POST",
-
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: this.$store.state.idToken,
-			},
-
-			body: JSON.stringify({
-				audio_ID: this.audio_ID,
-			}),
-		})
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				// console.log(data)
-				this.audioURL = "https://tile.loc.gov/storage-services/media/afc/cal/afc1986022_sr09b02.mp3"
-
-				this.wavesurfer.load(this.audioURL);
+		this.wavesurfer.load(this.audioURL);
 				
-			})
-			.catch((error) => {
-				console.error("Error:", error);
-			});
-
-
 		this.wavesurfer.on('error', function (err) {
 			console.warn("error", err?.message || err);
 		});
-		
+		this.wavesurfer.on('decode', function () {
+			console.log("audio has been loaded")
+		})
 		this.wavesurfer.on('ready', function () {
 			console.log("waveform should be viewable now")
 		});
@@ -137,13 +76,11 @@ export default {
 	position: relative;
 	margin-left: 0.2vw;
 	display: inline;
-	background: #334155;
 }
 
 .waveform {
 	height: 30vh;
 	width: 75px;
-	background: #dbeafe;
 
 }
 
