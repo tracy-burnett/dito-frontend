4a91e271ad0bb442df38f86c2adc6ad21b979f02
skysnolimit08 <59749584+skysnolimit08@users.noreply.github.com>
2023-08-20 20:50:42 +0800

    Update PlayerVertical.vue

--- src/components/PlayerVertical.vue
+++ src/components/PlayerVertical.vue
@@ -48,9 +48,9 @@
 	   </div>		
 			<!-- waveform display -->
 			<div class="flex flex-col h-[15vh] w-full">
-				<div id="waveform" ref="waveform" class="flex waveform h-[9vh] w-full"></div>
-				<div class="midwaveform w-full flex h-[1px]"></div>
-				<div id="miniwaveform" ref="miniwaveform" class="flex miniwaveform h-[3vh] w-full"></div>
+				<div id="waveform" ref="waveform" class="waveform h-4/5 w-full"></div>
+				<div class="midwaveform w-full h-[1px]"></div>
+				<div id="miniwaveform" ref="miniwaveform" class="miniwaveform h-1/5 w-full"></div>
 				<div class="absolute h-[15vh] z-10 content-center w-full flex flex-col py-[6vh] px-[1vw] text-sm"
 					style="background: #dbeafe;" v-if="loadingpercent > 0 && loadingpercent < 100">
 					<p>waveform {{ loadingpercent }}% completed</p>
