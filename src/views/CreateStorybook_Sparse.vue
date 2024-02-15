b9080204b7d7ca4293da77b0ac93f715f353063d
skysnolimit08 <skysnolimit08@gmail.com>
2024-02-15 10:36:18 -0800

    comment out debugging logs

--- src/views/CreateStorybook_Sparse.vue
+++ src/views/CreateStorybook_Sparse.vue
@@ -148,7 +148,7 @@ export default {
 			this.name = this.file["name"];
 			this.myArray = this.name.split(".");
 			this.ext = "." + this.myArray[this.myArray.length - 1];
-			console.log("before getting URL to use to send audio to server")
+			// console.log("before getting URL to use to send audio to server")
 			fetch(process.env.VUE_APP_api_URL + "s3/presignedposturl", {
 				method: "POST",
 				headers: {
@@ -184,7 +184,7 @@ export default {
 				.then(() =>
 					// post request to create new entry in audio table that includes data['audio_ID'], audio_URL (different from presigned URL), and other important information.
 					{
-						console.log("create new entry in audio table")
+						// console.log("create new entry in audio table")
 						fetch(process.env.VUE_APP_api_URL + "audio/", {
 							method: "POST",
 							headers: {
@@ -213,7 +213,7 @@ export default {
 									this.$store.commit("toggleInfobit", "InfoPublish");
 
 									if (this.int_title || this.int_text || this.int_language) {
-										console.log("creating interpretation")
+										// console.log("creating interpretation")
 										fetch(
 											process.env.VUE_APP_api_URL +
 												"interpretations/audio/" +
