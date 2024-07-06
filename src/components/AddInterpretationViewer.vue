b3c9d0db818323f336b8113934f99dc29cbaeef4
skysnolimit08 <skysnolimit08@gmail.com>
2024-07-06 05:31:25 -0700

    Make two side buttons permanent--they'll just only do something if the site is ready in its various necessary ways.

--- src/components/AddInterpretationViewer.vue
+++ src/components/AddInterpretationViewer.vue
@@ -1,43 +1,34 @@
 <template>
 	<div class="fixed right-0 w-[100px] flex flex-col  justify-center top-[23vh]">
 		<!-- this is a button for creating a new Interpretation and having it displayed in its own column -->
-		<div
-			v-if="$store.state.user && tier=='project'"
-			class="flex flex-col items-left py-[2vh]"
-			id="create"
-			@click="show"
-			style="cursor:pointer"
-		>
+		<div v-if="tier == 'project'" class="flex flex-col items-left py-[2vh]" id="create"
+			@click="show" style="cursor:pointer">
 			<div class="w-[7.5vh]">
 				<img src="@/assets/icon_add_no_fill.svg" />
 			</div>
-			<p class="text-left md:text-base" :class="{ tibetan: $store.state.promptsObject.name=='བོད་ཡིག', nottibetan: $store.state.promptsObject.name!='བོད་ཡིག' }">{{$store.state.promptsObject.oCreateNewInterpretation}}</p>
+			<p class="text-left md:text-base"
+				:class="{ tibetan: $store.state.promptsObject.name == 'བོད་ཡིག', nottibetan: $store.state.promptsObject.name != 'བོད་ཡིག' }">
+				{{ $store.state.promptsObject.oCreateNewInterpretation }}</p>
 		</div>
 		<!-- this is a button for creating a new Interpretation and having it displayed in its own column -->
-		<div
-			v-if="$store.state.user && $store.state.audioDuration>0"
-			class="flex flex-col items-left py-[2vh]"
-			id="upload"
-			@click="upload"
-			style="cursor:pointer"
-		>
+		<div class="flex flex-col items-left py-[2vh]"
+			id="upload" @click="upload" style="cursor:pointer">
 			<div class="w-[7.5vh]">
 				<img src="@/assets/icon_add_no_fill.svg" />
 			</div>
-			<p class="text-left md:text-base" :class="{ tibetan: $store.state.promptsObject.name=='བོད་ཡིག', nottibetan: $store.state.promptsObject.name!='བོད་ཡིག' }">{{$store.state.promptsObject.oUploadInterpretationFile}}</p>
+			<p class="text-left md:text-base"
+				:class="{ tibetan: $store.state.promptsObject.name == 'བོད་ཡིག', nottibetan: $store.state.promptsObject.name != 'བོད་ཡིག' }">
+				{{ $store.state.promptsObject.oUploadInterpretationFile }}</p>
 		</div>
 		<!-- this is a button for adding another column that shows an Interpretation that you have access to but aren't yet viewing -->
-		<div
-			class="flex flex-col items-left pt-[2vh]"
-			v-show="interpretationsList[0]"
-			id="add"
-			@click="add"
-			style="cursor:pointer"
-		>
+		<div class="flex flex-col items-left pt-[2vh]" v-show="interpretationsList[0]" id="add" @click="add"
+			style="cursor:pointer">
 			<div class="w-[7.5vh]">
 				<img src="@/assets/icon_add_fill.svg" />
 			</div>
-			<p class="text-left md:text-base" :class="{ tibetan: $store.state.promptsObject.name=='བོད་ཡིག', nottibetan: $store.state.promptsObject.name!='བོད་ཡིག' }">{{$store.state.promptsObject.oAddAnotherConsole}}</p>
+			<p class="text-left md:text-base"
+				:class="{ tibetan: $store.state.promptsObject.name == 'བོད་ཡིག', nottibetan: $store.state.promptsObject.name != 'བོད་ཡིག' }">
+				{{ $store.state.promptsObject.oAddAnotherConsole }}</p>
 		</div>
 		<br />
 	</div>
@@ -57,20 +48,25 @@ export default {
 			default: [],
 		},
 	},
-	mounted() {},
+	mounted() { },
 	methods: {
 		show() {
+			if (this.$store.state.user)
+			{this.$emit("toggleInterpretationModal");}
+		},
+		show2() {
 			this.$emit("toggleInterpretationModal");
 		},
 		upload() {
-			this.$emit("toggleUploadIntModal");
+			if (this.$store.state.user && this.$store.state.audioDuration > 0)
+			{this.$emit("toggleUploadIntModal");}
 		},
 		add() {
 			if (this.interpretationsList[0]) {
 				let newID = this.interpretationsList[0].id; // prepare to display whichever interpretation was listed FIRST in the dropdown menu in a viewer
 				this.$emit("displayInterpretationID", newID); // tell the parent component that the interpretation with that ID is now displayed in a viewer, and should no longer be available in the dropdown menu
 			} else {
-				this.show();
+				this.show2();
 			}
 		},
 	},
@@ -78,24 +74,41 @@ export default {
 </script>
 
 <style scoped>
-
 .tibetan {
-	font-size: 1.25rem/* 14px */;
-    line-height: 1.75rem/* 20px */;
+	font-size: 1.25rem
+		/* 14px */
+	;
+	line-height: 1.75rem
+		/* 20px */
+	;
 }
+
 .nottibetan {
-	font-size: 0.875rem/* 14px */;
-    line-height: 1.25rem/* 20px */;
+	font-size: 0.875rem
+		/* 14px */
+	;
+	line-height: 1.25rem
+		/* 20px */
+	;
 }
+
 @media (min-width: 768px) {
-    .nottibetan {
-        font-size: 1rem/* 16px */;
-        line-height: 1.5rem/* 24px */;
-    }
+	.nottibetan {
+		font-size: 1rem
+			/* 16px */
+		;
+		line-height: 1.5rem
+			/* 24px */
+		;
+	}
+
 	.tibetan {
-	font-size: 1.5rem/* 14px */;
-    line-height: 2rem/* 20px */;
-}
+		font-size: 1.5rem
+			/* 14px */
+		;
+		line-height: 2rem
+			/* 20px */
+		;
+	}
 }
 </style>
-
