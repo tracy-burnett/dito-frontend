79a600c7c10065629179a5ed4590b4569cd4a3fc
skysnolimit08 <skysnolimit08@gmail.com>
2024-07-06 18:11:02 -0700

    refactor home page for mobile Canto usergroup

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,27 @@
 <template>
-	<div @click="openstorybook()"
+	<div
 		class="relative mb-[3vh] overflow-hidden transition-colors bg-white border-slate-300 border shadow-lg cursor-pointer hover:overflow-auto group card rounded-xl  hover:border-slate-400 hover:outline-2">
 		<div class="relative flex items-center justify-between bg-emerald-800 flex-cols-2">
 			<div class="absolute inset-0 z-10 opacity-0 bg-emerald-900 group-hover:opacity-100"></div>
 			<!-- <div class=""> -->
 			<!-- <img class="flex left-0 mt-[1vh] object-scale-down  w-1/3" :src="image" /> -->
 
-			<div class="z-20 flex mt-1 ml-[1vw] mb-2 text-xl text-gray-100 group-hover:text-gray-50">{{ title
-			}}</div>
+			<div @click="playAudio()"
+				class="z-20 flex mt-1 ml-[1vw] mb-2 text-xl text-gray-100 group-hover:text-gray-50">{{ title
+				}}</div>
 			<div
 				class=" shrink-0 relative z-20 my-[1vh] flex items-center justify-center w-[3vw] h-[3vw] mr-[1vw] rounded-full shadow-lg bg-cyan-700">
 				<div class="absolute inset-0 z-30 rounded-full shadow-lg opacity-0 bg-cyan-800 group-hover:opacity-100">
 				</div>
-				<img class="z-40 w-[2vw] h-[2vw] ml-1" src="@/assets/play.svg" />
+				<img class="z-40 w-[2vw] h-[2vw] ml-1 storybookicon" src="@/assets/white_book_icon.svg"
+					@click.self="openstorybook()" />
 			</div>
 
 			<!-- </div> -->
 		</div>
 		<!-- <hr class="mt-[1.4vh] border-lime-500"> -->
 
-		<div class="px-4 py-2">
+		<div class="px-4 py-2" @click="playAudio()">
 			<!-- <div class="mt-1 mb-2 text-xl font-bold " ref="storybooktitle">{{ title }}</div> -->
 			<p class="mb-3 text-xs text-gray-400">
 				{{ description }}
@@ -31,6 +33,8 @@
 </template>
 
 <script>
+
+import { getIdToken } from "firebase/auth";
 // import cardbanner from "@/assets/IMG_0478.jpg";
 // import browse from "@/assets/icon_detailed_browse.svg";
 
@@ -63,11 +67,52 @@ export default {
 		},
 	},
 
-	// mounted() {
+	async mounted() {
+		if (this.$store.state.user) {
+			// REFRESH ID TOKEN FIRST AND WAIT FOR IT
+			await getIdToken(this.$store.state.user)
+				.then((idToken) => {
+					this.$store.commit("SetIdToken", idToken);
+					// console.log(this.$store.state.idToken)
+				})
+				.catch((error) => {
+					// An error happened.
+					console.log("Oops. " + error.code + ": " + error.message);
+				});
+		}
 		// this.$refs.storybooktitle.scrollIntoView({ block: "center" });
-	// },
+	},
 
 	methods: {
+		playAudio() {
+			const apiUrl = process.env.VUE_APP_api_URL + "s3/presignedgeturl";
+			fetch(apiUrl, {
+				method: "POST",
+
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: this.$store.state.idToken,
+				},
+
+				body: JSON.stringify({
+					audio_ID: this.audio_ID,
+				}),
+			})
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
+					// console.log(data)
+					this.audioURL = data["url"];
+					var audio = new Audio(this.audioURL); // path to file
+					audio.play();
+
+				})
+				.catch((error) => {
+					console.error("Error:", error);
+				});
+		},
+
 		openstorybook() {
 			this.$router.push({
 				name: "Storybook",
@@ -100,4 +145,5 @@ export default {
 	top: calc(50% - 1.75rem);
 	right: 0;
 } */
+
 </style>
